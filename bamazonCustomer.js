0baf3e8651819a8ed69a242cd9fa6d791bc4566f
Will <wmccrory@gmail.com>
2018-02-22 17:27:18 -0600

    Adding initial javascript

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+//bamazon customer file//
+
+//dependencies//
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var Table = require("tty-table");
+var chalk = require("chalk");
+
+//table creation//
+
+//mysql requiremnts//
+
+//inquirer prompts//
+
+//app initiation//
+
+//testing area//
